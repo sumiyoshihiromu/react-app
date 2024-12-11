9cf30153af27c010b19a22af748fde4cb1363cd8
sumiyoshihiromu <sumihiro127@icloud.com>
2024-12-11 16:55:25 +0900

    ヘッダー作成

---
+++ .prettierrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true, // セミコロンをつける
+  trailingComma: 'es5', // 末尾カンマをつける
+  singleQuote: true, // シングルコーテーションを使う
+  printWidth: 80, // 1行の文字数
+  tabWidth: 2, // タブ幅
+};
--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  semi: true, // セミコロンをつける
-  trailingComma: "es5", // 末尾カンマをつける
-  singleQuote: true,  // シングルコーテーションを使う
-  printWidth: 80,     // 1行の文字数
-  tabWidth: 2         // タブ幅
-};
