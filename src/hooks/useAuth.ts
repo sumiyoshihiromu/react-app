9b89da6e499b6ede0c1c948f01780fc6e32a3a56
sumiyoshihiromu <sumihiro127@icloud.com>
2024-12-12 11:44:03 +0900

    メッセージのモーダルを作成

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,10 +2,14 @@ import axios from 'axios';
 import { useCallback, useState } from 'react';
 import { User } from '../types/api/user';
 import { useHistory } from 'react-router-dom';
+import { useMessage } from './useMessage';
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
+
   const [loading, setLoading] = useState(false);
+
   const login = useCallback(
     (id: string, password: string) => {
       setLoading(true);
@@ -21,17 +25,26 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (id === password && res.data) {
+            showMessage({ title: 'ログインしました。', status: 'success' });
             history.push('/home');
           } else {
-            alert('ユーザーが見つかりませんでした。');
+            showMessage({
+              title: 'ユーザーが見つかりませんでした。',
+              status: 'error',
+            });
           }
         })
-        .catch(() => alert('ログインできませんでした。'))
+        .catch(() =>
+          showMessage({
+            title: 'ログインできませんでした。',
+            status: 'error',
+          })
+        )
         .finally(() => {
           setLoading(false);
         });
     },
-    [history]
+    [history, showMessage]
   );
   return { login, loading };
 };
