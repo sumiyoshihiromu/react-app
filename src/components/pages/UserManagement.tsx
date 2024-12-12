69a475d8acf5416ac5f975f4806b9d1b7f66f61f
sumiyoshihiromu <sumihiro127@icloud.com>
2024-12-12 14:44:00 +0900

    ユーザー管理画面を作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,30 @@
-import { Wrap, WrapItem } from '@chakra-ui/react';
-import { memo } from 'react';
+import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
+import { memo, useEffect } from 'react';
 import { UserCard } from '../organisms/layout/user/UserCard';
+import { useAllUsers } from '../../hooks/useAllUsers';
 
 export const UserManagement = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+  useEffect(() => getUsers(), []);
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://picsum.photos/800"
-          userName="すみちゃん"
-          fullName="住吉 弘"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://picsum.photos/800"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
