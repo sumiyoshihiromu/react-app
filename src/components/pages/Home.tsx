be68973cef91e9d3c0237ec73b9148c58b4b096f
sumiyoshihiromu <sumihiro127@icloud.com>
2024-12-13 13:39:50 +0900

    おしゃれなホームページ作成

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,28 @@
-import { memo } from "react";
+import { Center, Text } from '@chakra-ui/react';
+import { useState, useEffect, memo } from 'react';
 
 export const Home = memo(() => {
-  return <p>ホームページです</p>
-})
+  const text = 'Welcome to ユーザー管理アプリ';
+  const [displayedText, setDisplayedText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (currentIndex < text.length) {
+        setDisplayedText(text.substring(0, currentIndex + 1));
+        setCurrentIndex(currentIndex + 1);
+      } else {
+        // 全文字表示後、タイマーを停止
+        clearInterval(timer);
+      }
+    }, 100);
+
+    return () => clearInterval(timer);
+  }, [currentIndex]);
+
+  return (
+    <Center h="100vh">
+      <Text fontSize="3em">{displayedText}</Text>
+    </Center>
+  );
+});
